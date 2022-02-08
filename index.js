a1d3648c8d453161096691891c5798e575ad4b99
Teodor <1109111@nvna.eu>
2022-02-08 08:54:24 +0200

    upload project

---
+++ index.js
@@ -0,0 +1,12 @@
+let sum =0;
+console.log(sum);
+const ime = "Teodor";
+console.log(ime);
+let array = [sum, ime];
+console.log(array);
+let object = {
+    sum: 20,
+    ime: "IVAN"
+
+}
+console.log(object);
